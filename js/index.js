d016bb2c19a51b782ddb89ab007a4fb9c0b08cf5
Allxy <allxy01@gmail.com>
2022-08-14 21:23:30 +0300

    feat: added new card popup

--- js/index.js
+++ js/index.js
@@ -1,15 +1,26 @@
 const profileName = document.querySelector(".profile__name");
 const profileStatus = document.querySelector(".profile__status");
 const profileEditButton = document.querySelector(".profile__edit-btn");
+const profileAddButton = document.querySelector(".profile__add-btn");
 
 const places = document.querySelector(".places");
 const placeTemplate = document.querySelector("#place-card");
 
-const popup = document.querySelector(".popup");
-const popupEditForm = popup.querySelector(".popup__form");
-const popupCloseButton = popup.querySelector(".popup__close-btn");
-const popupNameInput = popup.querySelector(".popup__input_type_name");
-const popupStatusInput = popup.querySelector(".popup__input_type_status");
+const allPopups = document.querySelectorAll(".popup");
+
+const popupEdit = document.querySelector("#popup-edit");
+const popupEditForm = popupEdit.querySelector(".popup__form");
+const popupEditCloseButton = popupEdit.querySelector(".popup__close-btn");
+const popupEditNameInput = popupEdit.querySelector(".popup__input_type_name");
+const popupEditStatusInput = popupEdit.querySelector(
+  ".popup__input_type_status"
+);
+
+const popupAdd = document.querySelector("#popup-addcard");
+const popupAddAddForm = popupAdd.querySelector(".popup__form");
+const popupAddCloseButton = popupAdd.querySelector(".popup__close-btn");
+const popupAddNameInput = popupAdd.querySelector(".popup__input_type_name");
+const popupAddLinkInput = popupAdd.querySelector(".popup__input_type_link");
 
 const initialCards = [
   {
@@ -38,46 +49,62 @@ const initialCards = [
   },
 ];
 
-function openPopup() {
+function openPopup(popup) {
   popup.classList.add("popup_opened");
 }
 
-function closePopup() {
+function closePopup(popup) {
   popup.classList.remove("popup_opened");
 }
 
 function createNewCard(name, imgLink) {
   const clone = placeTemplate.content.cloneNode(true);
-  clone.querySelector(".place__img").style = `background-image: url(${imgLink})`;
-  clone.querySelector(".place__title").textContent = name;
+  const cloneImg = clone.querySelector(".place__img");
+  const cloneTitle = clone.querySelector(".place__title");
+
+  cloneImg.style = `background-image: url(${imgLink})`;
+  cloneTitle.textContent = name;
+
   return clone;
 }
 
 function clickEditCallback() {
-  popupNameInput.value = profileName.textContent;
-  popupStatusInput.value = profileStatus.textContent;
-  openPopup();
+  popupEditNameInput.value = profileName.textContent;
+  popupEditStatusInput.value = profileStatus.textContent;
+  openPopup(popupEdit);
 }
 
-function clickOutCallback(event) {
-  if (event.target.classList.contains("popup_opened")) {
-    closePopup();
-  }
-}
-
-function formSubmitCallback(event) {
+function popupEditFormCallback(event) {
   event.preventDefault();
-  profileName.textContent = popupNameInput.value;
-  profileStatus.textContent = popupStatusInput.value;
-  closePopup();
+  profileName.textContent = popupEditNameInput.value;
+  profileStatus.textContent = popupEditStatusInput.value;
+  closePopup(popupEdit);
 }
 
-initialCards.forEach(el => {
-  places.append(createNewCard(el.name, el.link))
-})
+function popupAddFormCallback(event) {
+  event.preventDefault();
+  const card = createNewCard(popupAddNameInput.value, popupAddLinkInput.value);
+  popupAddNameInput.value = "";
+  popupAddLinkInput.value = "";
+  places.append(card);
+  closePopup(popupAdd);
+}
+
+initialCards.forEach((el) => {
+  places.append(createNewCard(el.name, el.link));
+});
+
+[...allPopups].forEach(popup => addEventListener("click", (event) => {
+  if (event.target.classList.contains("popup_opened")) {
+    closePopup(popup);
+  }
+}));
 
 profileEditButton.addEventListener("click", clickEditCallback);
+profileAddButton.addEventListener("click", () => openPopup(popupAdd));
 
-popup.addEventListener("click", clickOutCallback);
-popupCloseButton.addEventListener("click", closePopup);
-popupEditForm.addEventListener("submit", formSubmitCallback);
+popupEditCloseButton.addEventListener("click", () => closePopup(popupEdit));
+popupAddCloseButton.addEventListener("click", () => closePopup(popupAdd));
+
+popupEditForm.addEventListener("submit", popupEditFormCallback);
+popupAddAddForm.addEventListener("submit", popupAddFormCallback);
