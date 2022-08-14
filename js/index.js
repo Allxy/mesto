426b6eb0b1e647dd13c036474c8d40d41ca97b37
Allxy <Allxy01@gmail.com>
2022-08-14 20:20:45 +0300

    feat: push images in places from js

--- js/index.js
+++ js/index.js
@@ -2,11 +2,41 @@ const profileName = document.querySelector(".profile__name");
 const profileStatus = document.querySelector(".profile__status");
 const profileEditButton = document.querySelector(".profile__edit-btn");
 
+const places = document.querySelector(".places");
+const placeTemplate = document.querySelector("#place-card");
+
 const popup = document.querySelector(".popup");
 const popupEditForm = popup.querySelector(".popup__form");
 const popupCloseButton = popup.querySelector(".popup__close-btn");
 const popupNameInput = popup.querySelector(".popup__input_type_name");
 const popupStatusInput = popup.querySelector(".popup__input_type_status");
+
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
 
 function openPopup() {
   popup.classList.add("popup_opened");
@@ -16,16 +46,23 @@ function closePopup() {
   popup.classList.remove("popup_opened");
 }
 
-function clickOutCallback(event) {
-  if (event.target.classList.contains("popup_opened")) {
-    closePopup();
-  }
+function createNewCard(name, imgLink) {
+  const clone = placeTemplate.content.cloneNode(true);
+  clone.querySelector(".place__img").style = `background-image: url(${imgLink})`;
+  clone.querySelector(".place__title").textContent = name;
+  return clone;
 }
 
 function clickEditCallback() {
   popupNameInput.value = profileName.textContent;
   popupStatusInput.value = profileStatus.textContent;
   openPopup();
+}
+
+function clickOutCallback(event) {
+  if (event.target.classList.contains("popup_opened")) {
+    closePopup();
+  }
 }
 
 function formSubmitCallback(event) {
@@ -35,7 +72,12 @@ function formSubmitCallback(event) {
   closePopup();
 }
 
+initialCards.forEach(el => {
+  places.append(createNewCard(el.name, el.link))
+})
+
+profileEditButton.addEventListener("click", clickEditCallback);
+
 popup.addEventListener("click", clickOutCallback);
 popupCloseButton.addEventListener("click", closePopup);
-profileEditButton.addEventListener("click", clickEditCallback);
 popupEditForm.addEventListener("submit", formSubmitCallback);
